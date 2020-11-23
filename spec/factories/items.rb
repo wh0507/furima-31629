ce840b93e36ee24b4a93bb984aa1408b1ad7afe3
wh0507 <banggu0507@gmail.com>
2020-11-23 17:04:48 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,15 +1,16 @@
 FactoryBot.define do
   factory :item do
-    id { '1' }
-    user
+    id                    { 1 }
     item_name             { '商品' }
     description           { '商品説明' }
-    category_id           { '2' }
-    condition_id          { '2' }
-    postage_payer_id      { '2' }
-    postage_area_id       { '2' }
-    postage_day_id        { '2' }
-    item_price            { 50000 } 
+    category_id           { 2 }
+    condition_id          { 2 }
+    postage_payer_id      { 2 }
+    postage_area_id       { 2 }
+    postage_day_id        { 2 }
+    item_price            { 50000 }
+
+    association :user
 
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
