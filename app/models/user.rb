eec19c5573470ad92602c11450db0067f66f13ce
wh0507 <banggu0507@gmail.com>
2020-12-10 00:04:08 +0900

    日本語対応更新

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
     validates :nickname, uniqueness: { case_sensitive: false }
     validates :birthday
     validates :email,    uniqueness: { case_sensitive: false }
-    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Include both letters and numbers' }
+    validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'を文字と数字の両方を含めて入力してください' }
 
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i, message: 'Full-width characters' } do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/i, message: '(全角)を入力してくだい' } do
       validates :last_name
       validates :first_name
     end
 
-    with_options format: { with: /\A[ァ-ン一]+\z/i, message: 'Full-width katakana characters' } do
+    with_options format: { with: /\A[ァ-ン一]+\z/i, message: '(全角)を入力してくだい' } do
       validates :last_name_kana
       validates :first_name_kana
     end
